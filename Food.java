3af28cf8ccb66bcfb5c62010940701eb65718284
lorrico1 <44854354+lorrico1@users.noreply.github.com>
2018-12-01 18:27:48 -0600

    Create Food.java

---
+++ Food.java
@@ -0,0 +1,12 @@
+package snakegame;
+import java.util.Random;
+public class Food {
+int foodx;
+int foody;
+Random random = new Random();
+public Food(int Y, int X) {
+	foodx = X;
+	foody = Y;
+}
+
+}
